dceaccc7e9e26cd8cfdd07e1aab418c0dfdabdac
danielpinto-developer <danielpinto.developer@gmail.com>
2025-02-19 21:00:46 -0600

    fix html filtering

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ DB_NAME = "upwork_jobs.db"
 def get_recent_jobs():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
-
-    # Only fetch jobs posted within the last 2 hours
     cursor.execute("""
         SELECT title, job_link, posted_time 
         FROM jobs 
-        WHERE posted_time LIKE '%%hour%' OR posted_time LIKE '%minute%'
-        ORDER BY id DESC
+        WHERE posted_time LIKE '%hour%' OR posted_time LIKE '%minute%'
+        ORDER BY CASE
+            WHEN posted_time LIKE '%minute%' THEN CAST(SUBSTR(posted_time, 1, INSTR(posted_time, ' ') - 1) AS INTEGER)
+            WHEN posted_time LIKE '%hour%' THEN CAST(SUBSTR(posted_time, 1, INSTR(posted_time, ' ') - 1) AS INTEGER) * 60
+            ELSE 9999
+        END ASC
     """)
     jobs = cursor.fetchall()
     conn.close()
